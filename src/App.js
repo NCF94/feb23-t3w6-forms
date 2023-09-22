78ab9d868e5285cdb765628fe8b2f9371e488129
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-09-22 10:52:16 +1000

    feat: export BusinessCardParent

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BusinessCardParent />
+      <BusinessCardParent />
+      <BusinessCardParent />
+      <BusinessCardParent />
+      <BusinessCardParent />
+      <BusinessCardParent />
+      <BusinessCardParent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/BusinessCardParent.jsx
@@ -0,0 +1,26 @@
+import {Component} from "react";
+
+import BusinessCardDisplay from "./BusinessCardDisplay";
+
+export default class BusinessCardParent extends Component {
+    constructor(){
+        super();
+    }
+
+    render (){
+        return(
+            <div>
+                <BusinessCardDisplay></BusinessCardDisplay>
+            </div>
+        )
+    }
+}
+
+
+
+// import react from "react";
+// class BlahBlah extends react.Component {
+//     constructor(){
+
+//     }
+// }
