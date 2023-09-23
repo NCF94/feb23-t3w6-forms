7cbf8a98d96b753e659bbcfd8cc344eecaed7246
Nicholas Ward <102345071+NCF94@users.noreply.github.com>
2023-09-23 11:45:30 +1000

    feat: nav bar with nav links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
 
 // React Router parts:
 // - BrowserRouter needs to exist higher up in the component tree than whatever we are writing Routes in
 // - Routes need to exist, determine what content is rendered on which route eg. /about for <About /> 
 // - Route specifies URL and component, belongs to Routes
+import { Outlet, Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+
+       <h1>This h1 shows on every page</h1>
+      <Navbar />
+
       <Routes>
-        <Route path="/" element ={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
 
-        <Route path="/about" element={<AboutPage />} />
+        {/* route for /about opening  */}
+        {/* <Route path="/about" element={<AboutPage />}> */}
 
+          {/* route for /about/contact */}
+          {/* <Route path="/about/contact" element={<ContactPage />} /> */}
+
+        {/* route for /about closing  */}
+        {/* </Route> */}
+        {/* <Route path="/" element={<HomePage />} /> */}
+
+        {/* <Route path="about" element={<AboutPage />}> */}
+
+          {/* route for /about/contact */}
+          {/* <Route path="contact" element={<ContactPage />} /> */}
+
+          {/* route for /about closing  */}
+
+        {/* route for /about closing  */}
+        {/* </Route> */}
+
+        <Route path="/about" element={<Outlet />}>
+          <Route index element={<AboutPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
       </Routes>
-      
+
     </div>
   );
 }
